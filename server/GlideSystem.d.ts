ca3d7ee387e6830e453a925fcc4794ec375b04d4
bbarber9 <breyton.barber@gmail.com>
2019-09-11 16:57:08 -0600

    Update GlideSystem

--- server/GlideSystem.d.ts
+++ server/GlideSystem.d.ts
@@ -1,4 +1,29 @@
 import { SNAPIGlideSystem } from "./SNAPIGlideSystem";
+type printable = string | number;
 declare class GlideSystem extends SNAPIGlideSystem {
+  info(
+    message: string,
+    parm1?: printable,
+    parm2?: printable,
+    parm3?: printable,
+    parm4?: printable,
+    parm5?: printable
+  ): void;
+  error(
+    message: string,
+    parm1?: printable,
+    parm2?: printable,
+    parm3?: printable,
+    parm4?: printable,
+    parm5?: printable
+  ): void;
+  warn(
+    message: string,
+    parm1?: printable,
+    parm2?: printable,
+    parm3?: printable,
+    parm4?: printable,
+    parm5?: printable
+  ): void;
 }
 export { GlideSystem };
