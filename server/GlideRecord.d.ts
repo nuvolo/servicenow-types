a2751db5bd38859ecf0b96ba1e4456637a25d5d1
bbarber9 <breyton.barber@gmail.com>
2019-09-24 17:02:12 -0600

    Add fix for overloaded get in GlideRecord

--- server/GlideRecord.d.ts
+++ server/GlideRecord.d.ts
@@ -1,5 +1,17 @@
 import { SNAPIGlideRecord } from "./SNAPIGlideRecord";
 declare class GlideRecord extends SNAPIGlideRecord {
+  /**
+   * Returns the specified record in an instantiated GlideRecord object.
+   * @param sys_id sys_id of the record to get.
+   */
   get(sys_id: string): boolean;
+  /**
+   * Returns the specified record in an instantiated GlideRecord object.
+   * @param fieldName Name of the instantiated GlideRecord column to search for the
+   * specified value parameter. If only a single parameter is
+   * passed in, the method assumes that this parameter is sys_id.
+   * @param value Value to match.
+   */
+  get(fieldName: string, value: string): boolean;
 }
 export { GlideRecord };
