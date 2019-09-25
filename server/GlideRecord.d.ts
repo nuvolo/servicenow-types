d4eb53cf0832770e823f359a79f9a1d7b1a059e5
bbarber9 <breyton.barber@gmail.com>
2019-09-25 12:55:16 -0600

    Fixbug with params on query in GlideRecord

--- server/GlideRecord.d.ts
+++ server/GlideRecord.d.ts
@@ -13,5 +13,22 @@ declare class GlideRecord extends SNAPIGlideRecord {
    * @param value Value to match.
    */
   get(fieldName: string, value: string): boolean;
+  /**
+   * Runs the query against the table based on the filters specified by addQuery,
+   * addEncodedQuery, etc.
+   * @param field Column name to query on.
+   * @param value Value to query for.
+   */
+  query(): void;
+  query(name: any, value: any): void;
+  /**
+   * Identical to query(). This method is intended to be used on tables
+   * where there is a column named query, which would interfere with using the
+   * query() method.
+   * @param name Column name on which to query
+   * @param value Value for which to query
+   */
+  _query(): void;
+  _query(name: any, value: any): void;
 }
 export { GlideRecord };
