f372ded41475159856434258a4267959fa4b1583
Ryan Chandler <ryan.chandler@nuvolo.com>
2019-12-05 13:20:32 -0700

    Added dateNumericValue function to GlideDuration

--- server/GlideDuration.d.ts
+++ server/GlideDuration.d.ts
@@ -1,3 +1,8 @@
 import { SNAPIGlideDuration } from "./SNAPIGlideDuration";
-declare class GlideDuration extends SNAPIGlideDuration {}
+declare class GlideDuration extends SNAPIGlideDuration {
+    /**
+   * Gets the duration in milliseconds.
+   */
+  dateNumericValue(): number
+}
 export { GlideDuration };
