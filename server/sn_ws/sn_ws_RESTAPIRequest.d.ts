653cf55ad0040916b271216fdb670eaddc447cb8
Kristiyan Nikolov <kristiyan.nikolov@nuvolo.com>
2020-01-30 15:45:35 +0200

    Generic types in util are removed and the originals are made generic.

--- server/sn_ws/sn_ws_RESTAPIRequest.d.ts
+++ server/sn_ws/sn_ws_RESTAPIRequest.d.ts
@@ -1,3 +1,7 @@
-import { sn_ws_SNAPIRESTAPIRequest } from "./sn_ws_SNAPIRESTAPIRequest";
-declare class sn_ws_RESTAPIRequest extends sn_ws_SNAPIRESTAPIRequest {}
+import { sn_ws_SNAPIRESTAPIRequest } from './sn_ws_SNAPIRESTAPIRequest';
+import { sn_ws_RESTAPIRequestBody } from './sn_ws_RESTAPIRequestBody';
+
+declare class sn_ws_RESTAPIRequest<T> extends sn_ws_SNAPIRESTAPIRequest {
+  body: sn_ws_RESTAPIRequestBody<T>;
+}
 export { sn_ws_RESTAPIRequest };
--- server/sn_ws/sn_ws_RESTAPIRequestBody.d.ts
+++ server/sn_ws/sn_ws_RESTAPIRequestBody.d.ts
@@ -1,3 +1,7 @@
-import { sn_ws_SNAPIRESTAPIRequestBody } from "./sn_ws_SNAPIRESTAPIRequestBody";
-declare class sn_ws_RESTAPIRequestBody extends sn_ws_SNAPIRESTAPIRequestBody {}
+import { sn_ws_SNAPIRESTAPIRequestBody } from './sn_ws_SNAPIRESTAPIRequestBody';
+declare class sn_ws_RESTAPIRequestBody<
+  T
+> extends sn_ws_SNAPIRESTAPIRequestBody {
+  data: T;
+}
 export { sn_ws_RESTAPIRequestBody };
