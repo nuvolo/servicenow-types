5c64f60cc4f237ad47749582c3bcd80e86ff5649
atanas dimitrov <atanas.dimitrov@2218-BGR.local>
2020-03-06 19:07:11 +0200

    REPM-19 Typo fix

--- server/GlideAction.ts
+++ server/GlideAction.ts
@@ -3,7 +3,7 @@ declare class GlideAction {
    * Set getRedirectURL in UI Actions.
    * @param scope takes the 'current' object.
    */
-  getRedirectURL<TRecord>(scope: TRecord): void;
+  setRedirectURL<TRecord>(scope: TRecord): void;
 }
 
 export { GlideAction };
