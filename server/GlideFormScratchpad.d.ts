4fb1174256010895fbaff9ad4d6f7150172b6cb0
atanas dimitrov <atanas.dimitrov@2218-BGR.local>
2020-03-06 15:18:41 +0200

    REPM-19 adding GlideFormScratchpad.d.ts

--- server/GlideFormScratchpad.d.ts
+++ server/GlideFormScratchpad.d.ts
@@ -1,3 +1,5 @@
-import { SNAPIGlideFormScratchpad } from "./SNAPIGlideFormScratchpad";
-declare class GlideFormScratchpad extends SNAPIGlideFormScratchpad {}
+import { SNAPIGlideFormScratchpad } from './SNAPIGlideFormScratchpad';
+declare class GlideFormScratchpad extends SNAPIGlideFormScratchpad {
+  [name: string]: any;
+}
 export { GlideFormScratchpad };
