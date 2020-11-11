0f27452fd1214b9c04e0853bbc768899f61f96bc
collin parker <collin.parker@nuvolo.com>
2020-11-11 12:31:27 -0800

    added types to sn_impex.d.ts

--- server/sn_impex.d.ts
+++ server/sn_impex.d.ts
@@ -1,3 +1,9 @@
 export {
   sn_impex_GlideExcelParser as GlideExcelParser
 } from './sn_impex/sn_impex_GlideExcelParser';
+export {
+  sn_impex_GlideImportSetTable
+} from './sn_impex/sn_impex_GlideImportSetTable'
+export {
+  sn_impex_GlideImportSetTransformMap
+} from './sn_impex/sn_impex_GlideImportSetTransformMap';
