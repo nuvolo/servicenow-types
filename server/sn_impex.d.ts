1fa0a4f8fb9cc6a8a89c19099b5b4cf1bfd0d832
collin parker <collin.parker@nuvolo.com>
2020-11-11 12:39:01 -0800

    added missing aliases for sn_impex classes

--- server/sn_impex.d.ts
+++ server/sn_impex.d.ts
@@ -2,8 +2,8 @@ export {
   sn_impex_GlideExcelParser as GlideExcelParser
 } from './sn_impex/sn_impex_GlideExcelParser';
 export {
-  sn_impex_GlideImportSetTable
+  sn_impex_GlideImportSetTable as GlideImportSetTable
 } from './sn_impex/sn_impex_GlideImportSetTable'
 export {
-  sn_impex_GlideImportSetTransformMap
+  sn_impex_GlideImportSetTransformMap as GlideImportSetTransformMap
 } from './sn_impex/sn_impex_GlideImportSetTransformMap';
